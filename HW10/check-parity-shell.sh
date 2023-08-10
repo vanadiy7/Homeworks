3570061cb0d506c3d8b2d92d7c66e3372d2d7a12
Vladimir Bobko <babaika.ko@gmail.com>
2023-08-10 14:52:22 +0300

    Delete HW10

--- HW10/check-parity-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-var=$1
-if [[ ! $var =~ ^[+-]?[0-9]+$ ]]; then
-    echo "${var} - это не число"
-elif [[ $((var % 2)) -eq 0 ]]; then
-    echo "${var} - четная"
-else
-    echo "${var} - нечетная"
-fi
--- HW10/print-value.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#1
-$VALUE=10
-echo $VALUE
-#2. Делаю экспорт export VALUE=15
-echo $VALUE
-#3. Сразу при запуске скрипта VALUE=20 ./print-value.sh
-echo $VALUE
