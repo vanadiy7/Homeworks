f38e1157ef244667dc2dc09ee93d94b792106bb9
Vladimir Bobko <babaika.ko@gmail.com>
2024-02-04 13:47:02 +0300

    Update main.tf

--- .infra/main.tf
+++ .infra/main.tf
@@ -8,3 +8,20 @@ data "google_compute_network" "default" {
   name = "default"
 }
 
+resource "google_storage_bucket" "remote-backend" {
+  project                     = var.project
+  name                        = bobko-diplom
+  uniform_bucket_level_access = true
+  location                    = var.region
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "google_storage_bucket_iam_member" "remote" {
+  bucket = google_storage_bucket.remote-backend.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:github-action-diplom@singular-glow-405611.iam.gserviceaccount.com"
+}
+
