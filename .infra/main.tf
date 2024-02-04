64aa07f9e37b29c827a9f70f3cee1f76453dd155
Vladimir Bobko <babaika.ko@gmail.com>
2024-02-04 18:24:21 +0300

    Update main.tf

--- .infra/main.tf
+++ .infra/main.tf
@@ -4,10 +4,6 @@ provider "google" {
   credentials = file("service-account.json")
 }
 
-data "google_compute_network" "default" {
-  name = "default"
-}
-
 terraform {
   backend "gcs" {
     bucket      = "bobko-diplom"
@@ -30,80 +26,190 @@ resource "google_artifact_registry_repository_iam_member" "member" {
   member     = "serviceAccount:github-action-diplom@singular-glow-405611.iam.gserviceaccount.com"
 }
 
-module "gke" {
-  source                     = "terraform-google-modules/kubernetes-engine/google"
-  project_id                 = var.project_id
-  name                       = var.name
-  region                     = var.region
-  zones                      = var.zones
-  network                    = "default"
-  subnetwork                 = "default"
-  ip_range_pods              = ""
-  ip_range_services          = ""
-  http_load_balancing        = false
-  network_policy             = true
-  horizontal_pod_autoscaling = true
-  filestore_csi_driver       = false
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.main.name
 
-  node_pools = [
-    {
-      name                      = "default-node-pool"
-      machine_type              = var.machine_type
-      node_locations            = "us-central1-b,us-central1-c"
-      min_count                 = var.min_count
-      max_count                 = var.max_count
-      local_ssd_count           = var.disk_size_gb
-      disk_type                 = "pd-standard"
-      image_type                = "COS_CONTAINERD"
-      auto_repair               = true
-      auto_upgrade              = true
-      service_account           = var.service_account
-      preemptible               = false
-      initial_node_count        = var.initial_node_count
-    },
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_project_service" "compute" {
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "container" {
+  service = "container.googleapis.com"
+}
+
+resource "google_compute_network" "main" {
+  name                            = "main"
+  routing_mode                    = "REGIONAL"
+  auto_create_subnetworks         = false
+  mtu                             = 1460
+  delete_default_routes_on_create = false
+
+  depends_on = [
+    google_project_service.compute,
+    google_project_service.container
+  ]
+}
+
+resource "google_compute_subnetwork" "private" {
+  name                     = "private"
+  ip_cidr_range            = "10.0.0.0/18"
+  region                   = var.region
+  network                  = google_compute_network.main.id
+  private_ip_google_access = true
+
+  secondary_ip_range {
+    range_name    = "k8s-pod-range"
+    ip_cidr_range = "10.48.0.0/14"
+  }
+  secondary_ip_range {
+    range_name    = "k8s-service-range"
+    ip_cidr_range = "10.52.0.0/20"
+  }
+}
+
+resource "google_compute_router" "router" {
+  name    = "router"
+  region  = var.region
+  network = google_compute_network.main.id
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router_nat
+resource "google_compute_router_nat" "nat" {
+  name   = "nat"
+  router = google_compute_router.router.name
+  region = "us-central1"
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.private.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  nat_ips = [google_compute_address.nat.self_link]
+}
+
+resource "google_compute_address" "nat" {
+  name         = "nat"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
+
+  depends_on = [google_project_service.compute]
+}
+
+resource "google_container_cluster" "primary" {
+  name                     = "primary"
+  location                 = var.region
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  network                  = google_compute_network.main.self_link
+  subnetwork               = google_compute_subnetwork.private.self_link
+  logging_service          = "logging.googleapis.com/kubernetes"
+  monitoring_service       = "monitoring.googleapis.com/kubernetes"
+  networking_mode          = "VPC_NATIVE"
+
+  node_locations = [
+    "us-central1-b"
   ]
 
-  node_pools_oauth_scopes = {
-    all = []
-
-    default-node-pool = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-  }
-
-  node_pools_labels = {
-    all = {}
-
-    default-node-pool = {
-      default-node-pool = true
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    horizontal_pod_autoscaling {
+      disabled = false
     }
   }
 
-  node_pools_metadata = {
-    all = {}
-
-    default-node-pool = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
+  release_channel {
+    channel = "REGULAR"
   }
 
-  node_pools_taints = {
-    all = []
+  workload_identity_config {
+    workload_pool = "singular-glow-405611.svc.id.goog"
+  }
 
-    default-node-pool = [
-      {
-        key    = "default-node-pool"
-        value  = true
-        effect = "PREFER_NO_SCHEDULE"
-      },
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pod-range"
+    services_secondary_range_name = "k8s-service-range"
+  }
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+}
+
+resource "google_service_account" "kubernetes" {
+  account_id = "kubernetes"
+}
+
+resource "google_container_node_pool" "general" {
+  name       = "general"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-small"
+
+    labels = {
+      role = "general"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
+}
 
-  node_pools_tags = {
-    all = []
+resource "google_container_node_pool" "spot" {
+  name    = "spot"
+  cluster = google_container_cluster.primary.id
 
-    default-node-pool = [
-      "default-node-pool",
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    labels = {
+      team = "devops"
+    }
+
+    taint {
+      key    = "instance_type"
+      value  = "spot"
+      effect = "NO_SCHEDULE"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
