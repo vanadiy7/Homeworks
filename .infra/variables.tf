f2dbfe6322fd1bedc76a018d336e317d594d4cc5
Vladimir Bobko <babaika.ko@gmail.com>
2024-02-04 15:14:37 +0300

    Update variables.tf

--- .infra/variables.tf
+++ .infra/variables.tf
@@ -1,16 +1,64 @@
-variable "project" {
-  default = "singular-glow-405611"
-}
-
-variable "region" {
-  default = "us-central1"
-}
-
 variable "storage_class" {
-  default = "STANDARD"
+  type        = string
+  description = "The type of storage_class"
 }
 
 variable "cluster_name" {
-  description = "The name of the GKE Autopilot cluster"
-  default     = "autopilot-cluster-bobko"
+  type        = string
+  description = "The name of the GKE cluster"
+}
+
+variable "credentials" {
+  type        = string
+  description = "Location of the credential keyfile."
+}
+
+variable "project_id" {
+  type        = string
+  description = "The project ID to create the cluster."
+}
+
+variable "region" {
+  type        = string
+  description = "The region to create the cluster."
+}
+
+variable "zones" {
+  type        = list(string)
+  description = "The zones to create the cluster."
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the cluster."
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Type of the node compute engines."
+}
+
+variable "min_count" {
+  type        = number
+  description = "Minimum number of nodes in the NodePool. Must be >=0 and <= max_node_count."
+}
+
+variable "max_count" {
+  type        = number
+  description = "Maximum number of nodes in the NodePool. Must be >= min_node_count."
+}
+
+variable "disk_size_gb" {
+  type        = number
+  description = "Size of the node's disk."
+}
+
+variable "service_account" {
+  type        = string
+  description = "The service account to run nodes as if not overridden in `node_pools`. The create_service_account variable default value (true) will cause a cluster-specific service account to be created."
+}
+
+variable "initial_node_count" {
+  type        = number
+  description = "The number of nodes to create in this cluster's default node pool."
 }
