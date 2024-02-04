f15a4314f83e76982e5f227116d478733f2d0d57
Vladimir Bobko <babaika.ko@gmail.com>
2024-02-04 14:32:56 +0300

    Update main.tf

--- .infra/main.tf
+++ .infra/main.tf
@@ -8,21 +8,24 @@ data "google_compute_network" "default" {
   name = "default"
 }
 
-resource "google_storage_bucket" "remote-backend" {
-  project                     = var.project
-  name                        = bobko-diplom
-  uniform_bucket_level_access = true
-  location                    = var.region
-  storage_class               = var.storage_class
-
-  versioning {
-    enabled = true
+terraform {
+  backend "gcs" {
+    bucket      = "bobko-diplom"
+    prefix      = "terraform/state"
+    credentials = "service-account.json"
   }
 }
-
-resource "google_storage_bucket_iam_member" "remote" {
-  bucket = google_storage_bucket.remote-backend.name
-  role   = "roles/storage.admin"
-  member = "serviceAccount:github-action-diplom@singular-glow-405611.iam.gserviceaccount.com"
+resource "google_artifact_registry_repository" "my_repository" {
+  provider      = google
+  location      = var.region
+  repository_id = "bobko-diplom"
+  description   = "My repository"
+  format        = "DOCKER"
 }
 
+resource "google_artifact_registry_repository_iam_member" "member" {
+  provider   = google
+  repository = google_artifact_registry_repository.my_repository.bobko-diplom
+  role       = "roles/artifactregistry.admin"
+  member     = "serviceAccount:github-action-diplom@singular-glow-405611.iam.gserviceaccount.com"
+}
