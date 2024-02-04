866db1b774d63f6df3d78db9beb1585dca29bd42
Vladimir Bobko <babaika.ko@gmail.com>
2024-02-04 18:26:10 +0300

    Update variables.tf

--- .infra/variables.tf
+++ .infra/variables.tf
@@ -13,34 +13,9 @@ variable "zones" {
   description = "The zones to create the cluster."
 }
 
-variable "name" {
-  type        = string
-  description = "The name of the cluster."
-}
-
 variable "machine_type" {
   type        = string
   description = "Type of the node compute engines."
-}
-
-variable "min_count" {
-  type        = number
-  description = "Minimum number of nodes in the NodePool. Must be >=0 and <= max_node_count."
-}
-
-variable "max_count" {
-  type        = number
-  description = "Maximum number of nodes in the NodePool. Must be >= min_node_count."
-}
-
-variable "disk_size_gb" {
-  type        = number
-  description = "Size of the node's disk."
-}
-
-variable "service_account" {
-  type        = string
-  description = "The service account to run nodes as if not overridden in `node_pools`. The create_service_account variable default value (true) will cause a cluster-specific service account to be created."
 }
 
 variable "initial_node_count" {
